62564deeedb06248398af166aab93db202d0aee3
evy-goryunov <evy.goryunov@gmail.com>
2019-04-25 17:27:36 +0300

    третья задача

---
+++ NumberFraction/NumberFraction/Fraction.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFraction
+{
+	/// <summary>
+	/// класс дробей - рациональных чисел.
+	/// </summary>
+	class Fraction
+	{
+		public int numerator;
+		public int denominator;
+
+		/// <summary>
+		/// Пустой конструктор
+		/// </summary>
+		public Fraction() { } // для создания пустого объекта в который запишем результат операции
+
+		/// <summary>
+		/// конструктор класса Fraction
+		/// </summary>
+		/// <param name="numer"></param>
+		/// <param name="denomi"></param>
+		public Fraction(int numer, int denomi)
+		{
+			numerator = numer;
+			denominator = denomi;
+		}
+
+		/// <summary>
+		/// сложение дробей
+		/// </summary>
+		/// <param name="fr1"></param>
+		/// <param name="fr2"></param>
+		/// <returns></returns>
+		public static Fraction Adding(Fraction fr1, Fraction fr2) // метод ожидает 2 объекта типа Fraction
+		{
+			Fraction resultOfFractions = new Fraction(); // новый объект типа Fraction для хранения результата операции
+
+			if (fr1.denominator != fr2.denominator)
+			{
+				resultOfFractions.denominator = fr1.denominator * fr2.denominator;
+				resultOfFractions.numerator = fr1.numerator * fr2.denominator + fr2.numerator * fr1.denominator;
+			}
+			else
+			{
+				resultOfFractions.denominator = fr1.denominator;
+				resultOfFractions.numerator = fr1.numerator + fr2.numerator;
+			}
+
+			return resultOfFractions;
+		}
+
+		/// <summary>
+		/// вычитание дробей
+		/// </summary>
+		/// <param name="fr1"></param>
+		/// <param name="fr2"></param>
+		/// <returns></returns>
+		public static Fraction Subtracting(Fraction fr1, Fraction fr2)
+		{
+			Fraction resultOfFractions = new Fraction();
+
+			if (fr1.denominator != fr2.denominator)
+			{
+				resultOfFractions.denominator = fr1.denominator * fr2.denominator;
+				resultOfFractions.numerator = fr1.numerator * fr2.denominator - fr2.numerator * fr1.denominator;
+			}
+			else
+			{
+				resultOfFractions.denominator = fr1.denominator;
+				resultOfFractions.numerator = fr1.numerator - fr2.numerator;
+			}
+
+			return resultOfFractions;
+		}
+
+		/// <summary>
+		/// умножение дробей
+		/// </summary>
+		/// <param name="fr1"></param>
+		/// <param name="fr2"></param>
+		/// <returns></returns>
+		public static Fraction Multiplication(Fraction fr1, Fraction fr2)
+		{
+			Fraction resultOfFractions = new Fraction();
+
+			resultOfFractions.denominator = fr1.denominator * fr2.denominator;
+			resultOfFractions.numerator = fr1.numerator * fr2.numerator;
+
+			return resultOfFractions;
+		}
+	}
+}
---
+++ NumberFraction/NumberFraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Горюнов Евгений
+ * *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. 
+ * Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу, 
+ * демонстрирующую все разработанные элементы класса. 
+ * Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
+ * ArgumentException("Знаменатель не может быть равен 0");
+ * Добавить упрощение дробей.
+ */
+namespace NumberFraction
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Fraction fr1 = new Fraction(7, 12);
+			Fraction fr2 = new Fraction(6, 13);
+			Fraction fr3 = new Fraction();
+
+			fr3.numerator = Fraction.Adding(fr1, fr2).numerator;
+			fr3.denominator = Fraction.Adding(fr1, fr2).denominator;
+			Console.WriteLine(fr3.numerator + "/" + fr3.denominator);
+
+			fr3.numerator = Fraction.Subtracting(fr1, fr2).numerator;
+			fr3.denominator = Fraction.Subtracting(fr1, fr2).denominator;
+			Console.WriteLine(fr3.numerator + "/" + fr3.denominator);
+
+			fr3.numerator = Fraction.Multiplication(fr1, fr2).numerator;
+			fr3.denominator = Fraction.Subtracting(fr1, fr2).denominator;
+			Console.WriteLine(fr3.numerator + "/" + fr3.denominator);
+
+			Console.ReadKey();
+		}
+	}
+}
